645a8bf4ee00702d200f858331245034633b691b
pratamarizki22 <rizkipurnomo914@gmail.com>
2026-01-22 15:02:49 +0700

    Remove all files from repo to replace with new code

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-blue-200">
-      <Text>Hello World</Text>
-    </View>
-  );
-}
